a7222ade359ad99ba236d9d3f4c436f33df40ae0
imad zeineddine <imadzeineddine17@gmail.com>
2023-11-26 18:02:07 -0300

    first commit

---
+++ app/src/main/java/com/zaher/pinproject/sistema_de_ponto.java
@@ -0,0 +1,14 @@
+package com.zaher.pinproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sistema_de_ponto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sistema_de_ponto);
+    }
+}
